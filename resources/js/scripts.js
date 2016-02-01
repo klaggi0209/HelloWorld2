6e612cdc5c392cf7661042606af1aa0911b3f81c
Felix Dausch <felix.dausch@plentymarkets.com>
2016-02-01 18:22:15 +0100

    ADD dynamically built sidebar navigation

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,13 +1,20 @@
 (function($) {
 
+
     $(document).ready(function() {
+        $('#sidebar-nav .nav').buildSidebarNav();
+
+        $('body').scrollspy({
+            target: '#sidebar-nav',
+            offset: 70
+        });
 
         $('[data-scrollTo]').each(function(i, elem) {
             var $elem = $( elem );
 
             $elem.click( function( e ) {
                 var selector = $elem.attr('data-scrollTo');
-                var scrollOffset = parseInt( $elem.attr('data-scrollOffset') );
+                var scrollOffset = parseInt( $(selector).attr('data-scrollOffset') );
                 var targetOffset;
 
 
@@ -26,8 +33,58 @@
                     scrollTop: (targetOffset - scrollOffset)
                 }, 300);
             });
+
         });
 
     });
+
+    $.fn.buildSidebarNav = function()
+    {
+        var list = this;
+        $('h1').each(function(i, section) {
+            var $section = $(section);
+            var sectionId = $section.getIdForced();
+            var listItem = $('<li><a href="#' + sectionId + '">' + $section.text() + '</a></li>');
+
+            listItem.find('a').click(function(e) {
+                e.preventDefault();
+                $('html,body').animate({
+                    scrollTop: ($section.offset().top - 70)
+                }, 300);
+            });
+
+            var subList = $('<ul></ul>');
+            $section.nextUntil('h1').filter('h2').each(function(j, subSection) {
+                var $subSection = $(subSection);
+                var subSectionId = $subSection.getIdForced();
+                var subListItem = $( '<li><a href="#'+subSectionId+'">' + $subSection.text() + '</a></li>' );
+
+                subListItem.find('a').click(function(e) {
+                    e.preventDefault();
+                    $('html,body').animate({
+                        scrollTop: ($subSection.offset().top - 70)
+                    }, 300);
+                });
+
+                subList.append( subListItem );
+            });
+
+            if( subList.find('li').length > 0 ) {
+                listItem.append(subList);
+            }
+
+            list.append(listItem);
+        });
+    };
+
+    var idCount = 0;
+    $.fn.getIdForced = function() {
+        if( !this.attr('id') || this.attr('id').length <= 0 )
+        {
+            this.attr('id', 'element_'+idCount);
+            idCount++;
+        }
+        return this.attr('id');
+    }
 
 }(jQuery));
